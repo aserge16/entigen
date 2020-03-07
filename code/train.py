92bb6ff37409da503cab848af988e2eaf3aa277a
aserge16 <aleks.sergeev2@gmail.com>
2020-03-07 18:55:15 -0500

    Removed loading of model from train

--- code/train.py
+++ code/train.py
@@ -1,5 +1,5 @@
 from entigen import *
-from semval_data_process import *
+from semval_data_process import create_training_data
 from keras.models import model_from_json
 
 
@@ -11,36 +11,33 @@ num_words = 20000
 max_len = 100
 
 
-def train():
+def train(num_epochs):
     sent_train, sent_test, label_train, label_test, tokenizer = create_training_data(train_path,
                                                                                     test_path, 
                                                                                     num_words = num_words, 
-                                                                                    max_len=max_len)
+                                                                                    max_len=max_len,
+                                                                                    test_size=0.15)
     model = create_model(num_words=num_words, 
                         embedding_size=300, 
                         max_len=max_len, 
                         label_len=10,
                         word_index = tokenizer.word_index,
                         embedding_file = embedding_file)
-    model.fit(sent_train, label_train, epochs=5, batch_size = 40)
+    model.fit(sent_train, label_train, epochs=num_epochs, batch_size = 40)
+
+    print("Training completed")
+    print("Testing model...")
+    score = model.evaluate(sent_test, label_test, batch_size = 40)
+    print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+
+    print("Saving model to disk...")
 
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
         json_file.write(model_json)
     model.save_weights("model.h5")
-    print("Saved model to disk")
-
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights("model.h5")
-    print("Loaded model from disk")
-
-    loaded_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    score = loaded_model.evaluate(sent_test, label_test, batch_size = 120)
-    print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+    print("Model saved to disk")
 
 
 if __name__=='__main__':
-    train()
+    train(num_epochs=5)
