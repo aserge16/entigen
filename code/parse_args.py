82aa8e1255d39b3de16db9f9eeb5e1d54f00fe19
aserge16 <aleks.sergeev2@gmail.com>
2020-03-07 19:44:03 -0500

    Added arguement parser

---
+++ code/parse_args.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+import sys
+
+def parse_args():
+    parser = ArgumentParser()
+
+    # FILES
+    parser.add_argument("--model_path", default="./model.json",
+                        type=str, help="Model file path")
+    parser.add_argument("--train_path", default="./data/TRAIN_FILE.TXT",
+                        type=str, help="Train data file path")
+    parser.add_argument("--test_path", default="./data/TEST_FILE_FULL.TXT",
+                        type=str, help="Test data file path")
+    parser.add_argument("--data_path", default="./data/temp_sentences.txt",
+                        type=str, help="Processed data file path")
+
+    # VALUES
+    parser.add_argument("--max_len", default=100,
+                        type=int, help="Max sentence length in data")
+    parser.add_argument("--num_words", default=20000,
+                        type=int, help="Max number of words to track")
+
+    args = parser.parse_args()
+    return args
+
+
+ARGS = parse_args()
--- code/train.py
+++ code/train.py
@@ -1,28 +1,21 @@
 from entigen import *
+from parse_args import ARGS
 from semval_data_process import create_training_data
 from keras.models import model_from_json
 
 
-
-train_path = "./data/TRAIN_FILE.TXT"
-test_path = "./data/TEST_FILE_FULL.TXT"
-embedding_file = "./data/glove.6B.50d.txt"
-num_words = 20000
-max_len = 100
-
-
 def train(num_epochs):
-    sent_train, sent_test, label_train, label_test, tokenizer = create_training_data(train_path,
-                                                                                    test_path, 
-                                                                                    num_words = num_words, 
-                                                                                    max_len=max_len,
+    sent_train, sent_test, label_train, label_test, tokenizer = create_training_data(ARGS.train_path,
+                                                                                    ARGS.test_path, 
+                                                                                    num_words = ARGS.num_words, 
+                                                                                    max_len=ARGS.max_len,
                                                                                     test_size=0.15)
-    model = create_model(num_words=num_words, 
+    model = create_model(num_words=ARGS.num_words, 
                         embedding_size=300, 
-                        max_len=max_len, 
+                        max_len=ARGS.max_len, 
                         label_len=10,
                         word_index = tokenizer.word_index,
-                        embedding_file = embedding_file)
+                        embedding_file = None)
     model.fit(sent_train, label_train, epochs=num_epochs, batch_size = 40)
 
     print("Training completed")
